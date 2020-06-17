2fbbf33e6075f23ef3e151f5df38c25aecebf64d
邱贤锋 <2938814394@qq.com>
2020-06-17 21:31:54 +0800

    6月17日java基础知识复习

--- src/two/Double.java
+++ src/two/Double.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Double {
     public static void main(String[] args) {
         Scanner input=new Scanner(System.in);
-        String s1=input.next();
-        System.out.println(s1);
+        String s=input.next();
+        System.out.println(s);
        }
     }
