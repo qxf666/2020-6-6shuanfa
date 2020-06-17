cf524378b5ce3964d7acf33e638e7fb84917f08e
邱贤锋 <2938814394@qq.com>
2020-06-17 11:43:47 +0800

    1

--- src/two/Double.java
+++ src/two/Double.java
@@ -2,8 +2,11 @@ package two;
 
 public class Double {
     public static void main(String[] args) {
-
-        long time = System.currentTimeMillis();
-        System.out.println(time);
+       final double EPSILON=1e-14;
+       double a=1-0.1-0.2-0.3;
+       double b=0.4;
+       if(Math.abs(a-b)<=EPSILON){
+           System.out.println("a,b相等");
+       }
     }
 }
