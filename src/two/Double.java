15f376c102821657dfa3782a5c3c4862b10809d6
邱贤锋 <2938814394@qq.com>
2020-06-17 22:16:16 +0800

    6月17日java基础知识复习

--- src/two/Double.java
+++ src/two/Double.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 public class Double {
     public static void main(String[] args) {
        String a="adfd";
-        System.out.println(a.contains("a"));
+       String b="adv";
+       int  c=a.compareTo(b);
+        System.out.println(a.startsWith("ad"));
        }
     }
