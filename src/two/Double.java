9f5d5e6dad44236c1883ada7b62212e94c38c571
邱贤锋 <2938814394@qq.com>
2020-06-17 23:30:17 +0800

    6月17日java基础知识复习

--- src/two/Double.java
+++ src/two/Double.java
@@ -4,9 +4,7 @@ import java.util.Scanner;
 
 public class Double {
     public static void main(String[] args) {
-      String message="asfsdfdfs";
-      int a= message.lastIndexOf("s",5);
-
+      int a=Integer.parseInt("123");
         System.out.println(a);
        }
     }
