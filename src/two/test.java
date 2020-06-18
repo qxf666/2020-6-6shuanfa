3edb80f0f07e73e723bdd27a923442b1a5862ed2
邱贤锋 <2938814394@qq.com>
2020-06-18 11:44:59 +0800

    6月18日java基础知识复习

--- src/two/test.java
+++ src/two/test.java
@@ -6,27 +6,9 @@ import java.util.Scanner;
 
 public class test {
     public static void main(String[] args) {
-        Scanner input =new Scanner(System.in);
-        String lottery=""+(int)(Math.random()*10)+(int)(Math.random()*10);
-        char lottery1=lottery.charAt(0);
-        char lottery2=lottery.charAt(1);
-        System.out.print("please input your guess number:");
-        String guess=input.nextLine();
-        char guess1=guess.charAt(0);
-        char guess2=guess.charAt(1);
-        if(lottery.equals(guess)){
-            System.out.println("you win 3000$");
-        }
-        else if(lottery1==guess2&&lottery2==guess1){
-            System.out.println("you win 1500$");
-        }
-        else if(lottery1==guess1||lottery1==guess2 ||
-                lottery2==guess1||lottery2==guess2
-        ){
-            System.out.println("you win 500$");
-        }
-        else
-            System.out.println("sorry you don't have win");
-        System.out.println("the Lottery is:"+lottery);
+        double amount=12.2030204431;
+        int a=(int)(amount*100);
+        double b=a/100.00;
+        System.out.println(b);
     }
 }
