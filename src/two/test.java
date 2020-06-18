9d5252f8866485ed1e3b0ee9df894c83c3557147
邱贤锋 <2938814394@qq.com>
2020-06-18 11:46:31 +0800

    6月18日java基础知识复习

--- src/two/test.java
+++ src/two/test.java
@@ -7,8 +7,6 @@ import java.util.Scanner;
 public class test {
     public static void main(String[] args) {
         double amount=12.2030204431;
-        int a=(int)(amount*100);
-        double b=a/100.00;
-        System.out.println(b);
+        System.out.printf("%4.2f",amount);
     }
 }
