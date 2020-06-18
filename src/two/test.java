0f8950bf2debe124a860a85da86587d65ff348e8
邱贤锋 <2938814394@qq.com>
2020-06-18 10:51:08 +0800

    6月18日java基础知识复习

--- src/two/test.java
+++ src/two/test.java
@@ -4,11 +4,21 @@ import java.util.Scanner;
 
 public class test {
     public static void main(String[] args) {
-      String a="asdf.af.dfdf.dfd";
-      String[] b=a.split("\\.");
-      for (String i :b){
-          System.out.println(i);
-      }
+        Scanner input = new Scanner(System.in);
+        System.out.println("please system input a hex char:");
+        String answer = input.nextLine();
+        char ch = answer.charAt(0);
+        int result = 0;
+        if ('A'<ch && ch<'F') {
+            result = ch-'A'+10;
+            System.out.println("the number is:" + result);
+        } else if (Character.isDigit(ch)) {
+            result = ch-'0';
+            System.out.println("the number is:" + result);
+        } else {
+            System.out.println("Your input is error");
+            System.exit(1);
+        }
 
-       }
     }
+}
