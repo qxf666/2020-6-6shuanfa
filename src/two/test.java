35eb377b6edd9e39237cd247f82b6a07eaeaef7b
邱贤锋 <2938814394@qq.com>
2020-06-18 11:20:13 +0800

    6月18日java基础知识复习

--- src/two/test.java
+++ src/two/test.java
@@ -1,24 +1,32 @@
 package two;
 
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
 import java.util.Scanner;
 
 public class test {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.println("please system input a hex char:");
-        String answer = input.nextLine();
-        char ch = answer.charAt(0);
-        int result = 0;
-        if ('A'<ch && ch<'F') {
-            result = ch-'A'+10;
-            System.out.println("the number is:" + result);
-        } else if (Character.isDigit(ch)) {
-            result = ch-'0';
-            System.out.println("the number is:" + result);
-        } else {
-            System.out.println("Your input is error");
-            System.exit(1);
+        Scanner input =new Scanner(System.in);
+        String lottery=""+(int)(Math.random()*10)+(int)(Math.random()*10);
+        char lottery1=lottery.charAt(0);
+        char lottery2=lottery.charAt(1);
+        System.out.print("please input your guess number:");
+        String guess=input.nextLine();
+        char guess1=guess.charAt(0);
+        char guess2=guess.charAt(1);
+        if(lottery.equals(guess)){
+            System.out.println("you win 3000$");
         }
-
+        else if(lottery1==guess2&&lottery2==guess1){
+            System.out.println("you win 1500$");
+        }
+        else if(lottery1==guess1||lottery1==guess2 ||
+                lottery2==guess1||lottery2==guess2
+        ){
+            System.out.println("you win 500$");
+        }
+        else
+            System.out.println("sorry you don't have win");
+        System.out.println("the Lottery is:"+lottery);
     }
 }
