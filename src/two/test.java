731fefff9fc03144018228c3fce4c3df779c537e
邱贤锋 <2938814394@qq.com>
2020-06-18 15:56:54 +0800

    6月18日java基础知识复习

--- src/two/test.java
+++ src/two/test.java
@@ -6,7 +6,36 @@ import java.util.Scanner;
 
 public class test {
     public static void main(String[] args) {
-        double amount=12.2030204431;
-        System.out.printf("%4.2f",amount);
+        int count=1;
+        int number1;
+        int number2;
+        int countNumber;
+        int correctNumber=0;
+        String output="";
+        String jieguo;
+        long time1=System.currentTimeMillis();
+        Scanner input=new Scanner(System.in);
+        while(count<=5){
+            number1=(int)(Math.random()*10);
+            number2=(int)(Math.random()*10);
+            System.out.printf("what is %d -%d ?",number1,number2);
+            int result=number1-number2;
+            countNumber=input.nextInt();
+            if(countNumber==result){
+                System.out.println("You are correct!");
+                jieguo="correct";
+            }
+            else{
+                System.out.println("Your answer is wrong");
+                System.out.printf("%d-%d should be %d \n",number1,number2,result);
+                jieguo="wrong";
+            }
+            output+=number1+"-"+number2+"="+result+jieguo+"\n";
+            count++;
+        }
+        long time2=System.currentTimeMillis();
+        long time=(time2-time1)/1000;
+        System.out.println("Test time is"+time+"seconds");
+        System.out.println(output);
     }
 }
