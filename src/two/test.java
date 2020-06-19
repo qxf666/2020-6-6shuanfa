e518bcd270d52e11e5bf182dfdc064775119bf95
邱贤锋 <2938814394@qq.com>
2020-06-19 20:22:25 +0800

    6月18日java基础知识复习

--- src/two/test.java
+++ src/two/test.java
@@ -1,14 +1,25 @@
 package two;
 
+import java.util.Scanner;
+
 public class test {
     public static void main(String[] args) {
-        for (int i = 0; i < 8; i++) {
-            System.out.println(i+"j");
-            for (int j = 0; j < 9; j++) {
-                if ( j == 1)
-                    break;
-                System.out.println("j="+j);
-            }
-        }
+        Scanner input=new Scanner(System.in);
+        System.out.print("Enter a string:");
+        String s=input.nextLine();
+        int count=0;
+        boolean flag=true;
+       for(int i=0,j=s.length()-1;i<s.length()/2;i++,j--){
+           if(s.charAt(i)!=s.charAt(j)){
+               flag=false;
+               break;
+           }
+       }
+       if (flag){
+           System.out.println( "is palindorme");
+       }
+       else
+           System.out.println( "is not a palindorme");
+
     }
 }
