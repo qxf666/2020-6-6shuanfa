c91e9f872e4251e36c69d78668c7d1056003a5dc
邱贤锋 <2938814394@qq.com>
2020-06-20 21:03:35 +0800

    6月18日java基础知识复习

--- src/two/test4.java
+++ src/two/test4.java
@@ -2,9 +2,31 @@ package two;
 
 public class test4 {
     public static void main(String[] args) {
-        int[] a={1,2,3,4,5};
-        int[] b=new int[5];
-        System.arraycopy(a,0,b,0,a.length);
-        System.out.println(b[2]);
+        char[] array=new char[100];
+        int[] a=new int[26];
+        for(int i=0;i<100;i++){
+            array[i]=randomCharacter();
+        }
+        int count;
+        for (int i=0;i<100;i++){
+            count=array[i]-'a';
+            a[count]++;
+        }
+        char ch;
+        int times=0;
+        for (int i=0;i<26;i++){
+            ch=(char)(i+'a');
+            if (a[i]!=0) {
+                times++;
+                if(times%10==0)
+                    System.out.println(a[i] + " " + ch + " ");
+                else
+                    System.out.print(a[i] + " " + ch + " ");
+            }
+        }
+    }
+    public static char randomCharacter(){
+        int randomnumber=(int)(Math.random()*('z'-'a'+1))+'a';
+        return (char)randomnumber;
     }
 }
